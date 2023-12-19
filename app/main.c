e4ec12a917b7505f8303a83860840a4295c72fe0
Kevin Tony <kevinoct2005@gmail.com>
2023-12-19 01:15:15 +0100

    Add simstop feature

--- app/main.c
+++ app/main.c
@@ -47,6 +47,7 @@ int sim7020cmd_at(int arg, char **argv);
 int sim7020cmd_recv(int arg, char **argv);
 int sim7020cmd_reset(int arg, char **argv);
 int sim7020cmd_topic(int arg, char **argv);
+int sim7020cmd_stop(int argc, char **argv);
 #endif /* MODULE_SIM7020 */
 
 static const shell_command_t shell_commands[] = {
@@ -64,7 +65,8 @@ static const shell_command_t shell_commands[] = {
     { "uclose", "Close SIM7020 socket", sim7020cmd_close },
     { "utest", "repeat usend", sim7020cmd_test },                
     { "at", "run AT command, for example \"at AT+CSQ\"", sim7020cmd_at },
-    {"topic", "publish on topic", sim7020cmd_topic},
+    { "topic", "publish on topic", sim7020cmd_topic},
+    { "stop", "stop SIM7020 connection", sim7020cmd_stop},
 #endif /* MODULE_SIM7020 */
 #ifdef MODULE_MQTTSN_PUBLISHER
     { "mqstat", "print MQTT status", mqttsn_stats_cmd},
